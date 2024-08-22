7c55ea77747ac6a8707d11a811e16de3ca1f4d60
Valeria Rybakova <valerian.rybakova@yandex.ru>
2024-08-23 01:00:10 +0300

    feat: вёрстка блока с участниками (без анимации слайдера)

---
+++ data/constants.js
@@ -0,0 +1,8 @@
+export const memberCardConfig = {
+  listId: 'members-list',
+  templateId: 'member-template',
+  cardClass: '.member',
+  imageClass: '.member__image',
+  nameClass: '.member__name',
+  rankClass: '.member__rank',
+};
